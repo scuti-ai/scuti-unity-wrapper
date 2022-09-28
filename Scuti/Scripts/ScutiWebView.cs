00505b996fd4c11c5d918595e45b135ef9f7e531
Mark Grossnickle <markgrossnickle@users.noreply.github.com>
2022-09-28 15:40:04 -0600

    testing dimensions

--- Scuti/Scripts/ScutiWebView.cs
+++ Scuti/Scripts/ScutiWebView.cs
@@ -190,7 +190,7 @@ public class ScutiWebView : MonoBehaviour
         {
             var dst = System.IO.Path.Combine(Application.persistentDataPath, Url);
             var scriptUrl = GetURL();
-            string htmlContent = "<html><head><script src=\"XURLX\"></script>\n</head>\n<body style=\"margin: -10; overflow: hidden; padding: 0;\">\n    <div id=\"scuti-store\"></div>\n     <script>\n    (async function () {\n      await window.SCUTI_SDK.initialize(\"XAPPIDX\")\n      window.SCUTI_SDK.renderStore(\n        \"scuti-store\",\n        () => Unity.call(\'exit\'),\n        (payload) => Unity.call(\'exchange!\'+JSON.stringify(payload)),\n        { width: \'100%\', height: \'100%\' }\n      )\n    })()\n  </script>\n</body>\n</html>";
+            string htmlContent = "<html><head><script src=\"XURLX\"></script>\n</head>\n<body style=\"margin: -10; overflow: hidden; padding: 0;\">\n    <div id=\"scuti-store\"></div>\n     <script>\n    (async function () {\n      await window.SCUTI_SDK.initialize(\"XAPPIDX\")\n      window.SCUTI_SDK.renderStore(\n        \"scuti-store\",\n        () => Unity.call(\'exit\'),\n        (payload) => Unity.call(\'exchange!\'+JSON.stringify(payload)),\n        { width: \'400\', height: \'1200\' }\n      )\n    })()\n  </script>\n</body>\n</html>";
             htmlContent = htmlContent.Replace("XURLX", scriptUrl);
             htmlContent = htmlContent.Replace("XAPPIDX", ScutiNetClient.Instance.GameId);
             Debug.Log("ScutiNetClient : " + ScutiNetClient.Instance);
