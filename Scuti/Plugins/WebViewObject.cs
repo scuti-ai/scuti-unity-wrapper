42f7f34583a4ac9f76cef189dc9c7297d77ce8e3
Mark Grossnickle <markgrossnickle@users.noreply.github.com>
2023-04-18 11:58:25 -0600

    Logs for testing black screen

--- Scuti/Plugins/WebViewObject.cs
+++ Scuti/Plugins/WebViewObject.cs
@@ -887,6 +887,7 @@ public class WebViewObject : MonoBehaviour, IWebView
 
     public void EvaluateJS(string js)
     {
+        Debug.LogError("Eval JS: " + js);
 #if UNITY_WEBGL
 #if !UNITY_EDITOR
         _gree_unity_webview_evaluateJS(name, js);
